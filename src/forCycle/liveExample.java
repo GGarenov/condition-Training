420ce498fcab048692783f293b88d8444469485d
GGarenov <g_garenov@outlook.com>
2022-08-08 14:00:32 +0300

    Java basics

---
+++ src/forCycle/liveExample.java
@@ -0,0 +1,12 @@
+package forCycle;
+
+import java.util.Scanner;
+
+public class liveExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(i);
+        }
+    }
+}
